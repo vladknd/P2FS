7b1fc709c9133744337c4a06e1a1bce9ce301d22
vladknd <voznitsa98@gmail.com>
2024-04-18 04:19:31 -0400

    Aioconsole

--- client/main.py
+++ client/main.py
@@ -1,8 +1,8 @@
 import socket
 import asyncio
+import aioconsole
 import os 
 import threading
-import concurrent
 
 from c2c_controller import Client2ClientController
 
@@ -15,7 +15,7 @@ def startup():
     server_port = int(input("Enter the server port: "))
     return name, server_host, server_port
 
-async def run_user_interface(controller): 
+async def run_user_interface(loop, controller): 
     while True:
         print("\nAvailable Commands:")
         print("1. Register with server")
@@ -25,7 +25,8 @@ async def run_user_interface(controller):
         print("5. Request file from peer")
         print("6. Update contact information")
         print("7. Exit")
-        choice = input("Enter your choice: ")
+        # choice = await loop.run_in_executor(None, input, "Enter your choice: ")
+        choice = await aioconsole.ainput("Enter your choice: ")
 
         if choice == "1":
             print("Registering with server...")
@@ -40,21 +41,21 @@ async def run_user_interface(controller):
             file_choice = "y"
             file_names = []
             while file_choice == "y":
-                file_name = input("Enter file name: ")
+                file_name = await aioconsole.ainput("Enter file name: ")
                 file_names.append(file_name)
-                file_choice = input("Would you like to enter another file name? (y/n)")
+                file_choice = await aioconsole.ainput("Would you like to enter another file name? (y/n)")
             await asyncio.create_task(controller.request_to_server("REMOVE", file_names))
             print("Removing files from server...")
         elif choice == "5":
             print("Requesting file from peer...")
-            peer_ip = input("Enter peer IP: ")
-            peer_port = input("Enter peer port: ")
-            request_id = input("Enter request ID: ")
-            file_name = input("Enter file name: ")
+            peer_ip = await aioconsole.ainput("Enter peer IP: ")
+            peer_port = await aioconsole.ainput("Enter peer port: ")
+            request_id = await aioconsole.ainput("Enter request ID: ")
+            file_name = await aioconsole.ainput("Enter file name: ")
             await asyncio.create_task(controller.request_file(peer_ip=peer_ip, peer_port=int(peer_port), request_id=request_id, file_name=file_name))
         elif choice == "6":
-            ip_address = input("Enter the new IP address: ")
-            port = input("Enter the new port: ")
+            ip_address = await aioconsole.ainput("Enter the new IP address: ")
+            port = await aioconsole.ainput("Enter the new port: ")
             await asyncio.create_task(controller.request_to_server("UPDATE-CONTACT", ip_address=ip_address, udp_port=port))
             print("Updating contact...")
         elif choice == "7":
@@ -67,8 +68,6 @@ async def main():
     udp_bind_port = 3000
     # name, server_host, server_port = startup()
     loop = asyncio.get_running_loop()
-    # Create a default executor
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
     # udp_bind_port = int(os.getenv('PORT'))
     # print(f"UDP bind port: {udp_bind_port}")
     # local_ip = socket.gethostbyname(socket.gethostname())
@@ -77,6 +76,7 @@ async def main():
     udp = Client2ClientUDPCommunication(loop, tcp, local_ip, udp_bind_port)
     controller = Client2ClientController(loop, udp, tcp)
     await asyncio.create_task(udp.start_server()) 
+    await asyncio.create_task(run_user_interface(loop, controller))
     
 if __name__ == "__main__":
     asyncio.run(main())
